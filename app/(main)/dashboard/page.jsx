28f8e9ec0904328154017c853dadb94b554c9d51
AniketShankhwar <112162439+AniketShankhwar@users.noreply.github.com>
2025-05-14 22:07:50 +0530

    Update page.jsx

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -5,11 +5,10 @@ import DashboardView from "./_components/dashboard-view";
 
 const IndustryInsightsPage = async () => {
   const { isOnboarded } = await getUserOnboardingStatus();
-  const insights = await getIndustryInsights();
-
   if (!isOnboarded) {
     redirect("/onboarding");
   }
+  const insights = await getIndustryInsights();
 
   return(
   <div className="container mx-auto">
